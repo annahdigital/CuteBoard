40a3f742b6d9103f9046b7b1a7d15b78ba2b6f78
annahdigital <annah.borisova@gmail.com>
2019-12-11 11:45:06 +0300

    added popup window for start dialog.

--- app/src/main/java/com/example/cuteboard/MainActivity.java
+++ app/src/main/java/com/example/cuteboard/MainActivity.java
@@ -1,14 +1,107 @@
 package com.example.cuteboard;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private SwipeRefreshLayout mSwipeLayout;
+    private PopupWindow mPopupWindow;
+    private Context mContext;
+    private CoordinatorLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final FloatingActionButton addButton = this.findViewById(R.id.add_post);
+        addButton.setVisibility(View.GONE);
+
+
+
+        mContext = getApplicationContext();
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View customView = inflater.inflate(R.layout.start_page, null);
+        mPopupWindow = new PopupWindow(
+                customView,
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT,
+                true
+        );
+
+        mPopupWindow.setElevation(5.0f);
+        Button mStartButton = customView.findViewById(R.id.get_started);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPopupWindow.dismiss();
+                addButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.main_space).post(new Runnable() {
+            @Override
+            public void run() {
+                mPopupWindow.showAtLocation(findViewById(R.id.main_space), Gravity.CENTER,0,0);
+            }
+        });
+        
+        mRecyclerView = findViewById(R.id.post_view);
+        mSwipeLayout = findViewById(R.id.swipeRefreshLayout);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mSwipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //new FetchFeedTask().execute((Void) null);
+
+            }
+        });
     }
+
+   /* private void loadPhoto(ImageView imageView, int width, int height) {
+
+        ImageView tempImageView = imageView;
+
+
+        AlertDialog.Builder imageDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        View layout = inflater.inflate(R.layout.start_page,
+                (ViewGroup) findViewById(R.id.main_space));
+        ImageView image = (ImageView) layout.findViewById(R.id.catImage);
+        image.setImageDrawable(tempImageView.getDrawable());
+        imageDialog.setView(layout);
+        imageDialog.setPositiveButton(getString(R.string.start), new DialogInterface.OnClickListener(){
+
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+
+        });
+
+
+        imageDialog.create();
+        imageDialog.show();
+    }*/
 }
